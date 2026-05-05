748dd12df0a7bc9a5ee41b6671fecad0c2794a98
Midorinekonosakotu <midorineko10801@gmail.com>
2026-05-05 22:38:35 +0900

    左フリッパーの調整

---
+++ Assets/Scripts/FlipperController.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlipperController : MonoBehaviour
+{
+    [SerializeField] private FlipperInput _leftFlipper;
+    [SerializeField] private FlipperInput _rightFlipper;
+
+    private PinballInputActions _actions;
+
+    void Awake()
+    {
+        _actions = new PinballInputActions();
+    }
+
+    void OnEnable()
+    {
+        _actions.Enable();
+
+        // 左フリッパー
+        _actions.GamePlay.LeftFlipper.performed += OnLeftPerformed;
+        _actions.GamePlay.LeftFlipper.canceled += OnLeftCanceled;
+
+        // 右フリッパー
+        _actions.GamePlay.RightFlipper.performed += OnRightPerformed;
+        _actions.GamePlay.RightFlipper.canceled += OnRightCanceled;
+    }
+
+    void OnDisable()
+    {
+        _actions.Disable();
+
+        // 左フリッパー
+        _actions.GamePlay.LeftFlipper.performed -= OnLeftPerformed;
+        _actions.GamePlay.LeftFlipper.canceled -= OnLeftCanceled;
+
+        // 右フリッパー
+        _actions.GamePlay.RightFlipper.performed -= OnRightPerformed;
+        _actions.GamePlay.RightFlipper.canceled -= OnRightCanceled;
+    }
+
+    /// <summary>
+    /// 左フリッパーの処理
+    /// </summary>
+    /// <param name="ctx"></param>
+    private void OnLeftPerformed(InputAction.CallbackContext ctx)
+    {
+        //if (GameManager.Instance.State != GameState.Playing) return;
+
+        _leftFlipper.SetInput(true);
+    }
+
+    private void OnLeftCanceled(InputAction.CallbackContext ctx)
+    {
+        _leftFlipper.SetInput(false);
+    }
+
+    /// <summary>
+    /// 右フリッパーの処理
+    /// </summary>
+    /// <param name="ctx"></param>
+    private void OnRightPerformed(InputAction.CallbackContext ctx)
+    {
+        //if (GameManager.Instance.State != GameState.Playing) return;
+
+        _rightFlipper.SetInput(true);
+    }
+
+    private void OnRightCanceled(InputAction.CallbackContext ctx)
+    {
+        _rightFlipper.SetInput(false);
+    }
+}
---
+++ Assets/Scripts/FlipperInput.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+[RequireComponent (typeof(HingeJoint))]
+public class FlipperInput : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private HingeJoint joint;
+
+    [Header("Control")]
+    [SerializeField] private float torque = 2000f;
+    [SerializeField] private float impulseBoost = 1.8f;
+    [SerializeField] private float maxAngularVelocity = 60f;
+    [SerializeField] private float defaultAngle = 15f;
+
+    [Header("Debug")]
+    [SerializeField] private bool debugDraw = false;
+
+    private bool isPressed;
+    private bool justPressed;
+    [SerializeField] private bool isLeft;
+
+    void Awake()
+    {
+        if(!rb) rb = GetComponent<Rigidbody>();
+        if(!joint) joint = GetComponent<HingeJoint>();
+    }
+
+    void Start()
+    {
+        rb.maxAngularVelocity = maxAngularVelocity;
+    }
+
+    void FixedUpdate()
+    {
+        //if (GameManager.Instance.State != GameState.Playing) return;    // Playing 中のみ動作
+        Debug.Log(isPressed);
+
+        JointSpring spring = joint.spring;
+        float dir = isLeft ? -1f : 1f;
+
+        if (isPressed)
+        {
+            joint.useSpring = false;
+
+            rb.AddTorque(transform.up * torque * dir, ForceMode.Acceleration);
+
+            if (justPressed)
+            {
+                rb.AddTorque(transform.up * torque * impulseBoost * dir, ForceMode.Impulse);
+                justPressed = false;
+            }
+        }
+        else
+        {
+            joint.useSpring = true;
+
+            spring.targetPosition = defaultAngle;
+            joint.spring = spring;
+        }
+    }
+
+    public void SetInput(bool pressed)
+    {
+        if(pressed && !isPressed)
+        {
+            justPressed = true;
+        }
+
+        isPressed = pressed;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.TryGetComponent(out Rigidbody ball)) return;
+
+        Vector3 contactPoint = collision.contacts[0].point;
+
+        Vector3 velocity = rb.GetPointVelocity(contactPoint);
+
+        ball.AddForce(velocity * 0.8f, ForceMode.Impulse);
+
+        if(debugDraw)
+        {
+            Debug.DrawRay(contactPoint, velocity, Color.red, 1f);
+        }
+    }
+}
